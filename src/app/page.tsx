b1f38e7052c49287f8eb1cc2f1726580363128d8
tetty <th.12042.venom@gmail.com>
2024-07-11 18:20:48 +0900

    all the files added

---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+export const dynamic = 'force-dynamic'
+
+import * as React from 'react';
+import { useState } from 'react';
+import { Box, Typography } from '@mui/material';
+import TaskInput from '../sections/TaskInput';
+import TaskList from '../sections/TaskList';
+
+export default function App() {
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  function handleAddTask(task: string) {
+    setTasks([...tasks, task]);
+  }
+
+  function handleDeleteTask(index: number) {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  }
+
+  return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: '#f5f5f5',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 2,
+      }}
+    >
+      <Typography variant="h2" component="h1" gutterBottom align="center">
+        SelinのToDoリスト
+      </Typography>
+      <TaskInput onAddTask={handleAddTask} />
+      <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} />
+    </Box>
+  );
+}
